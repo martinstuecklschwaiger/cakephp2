07f64eb55522e97517b79d686903a674b5fe7081
Daniele Colangelo <daniele.colangelo@walls.io>
2024-02-06 15:35:56 +0100

    Change utf8_decode/decode via rector
    https://wiki.php.net/rfc/remove_utf8_decode_and_utf8_encode

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+use Rector\Php82\Rector\FuncCall\Utf8DecodeEncodeToMbConvertEncodingRector;
 use Rector\Transform\Rector\Class_\AddAllowDynamicPropertiesAttributeRector;
 use Rector\ValueObject\PhpVersion;
 
@@ -15,5 +16,6 @@ return RectorConfig::configure()
     ->withConfiguredRule(AddAllowDynamicPropertiesAttributeRector::class, ["*"])
     ->withRules([
         NullToStrictStringFuncCallArgRector::class,
+        Utf8DecodeEncodeToMbConvertEncodingRector::class,
     ])
     ->withPHPVersion(PhpVersion::PHP_83);
